e6097db304539b4e53fbcb07603e2622247d734e
Shay Peleg <shaypeleg1@gmail.com>
2017-01-15 16:39:37 +0200

    changed put function to recive _id inside body

--- server-full.js
+++ server-full.js
@@ -168,16 +168,15 @@ app.post('/data/:objType', upload.single('file'), function (req, res) {
 });
 
 // PUT - updates
-app.put('/data/:objType/:id',  function (req, res) {
+
+app.put('/data/:objType/',  function (req, res) {
 	const objType 	= req.params.objType;
-	const objId 	= req.params.id;
 	const newObj 	= req.body;
     if (newObj._id && typeof newObj._id === 'string') newObj._id = new mongodb.ObjectID(newObj._id);
-
-    cl(`Requested to UPDATE the ${objType} with id: ${objId}`);
+    cl(`Requested to UPDATE the ${objType} with id: ${newObj._id}`);
 	dbConnect().then((db) => {
 		const collection = db.collection(objType);
-		collection.updateOne({ _id:  new mongodb.ObjectID(objId)}, newObj,
+		collection.updateOne({ _id: newObj._id}, newObj,
 		 (err, result) => {
 			if (err) {
 				cl('Cannot Update', err)
